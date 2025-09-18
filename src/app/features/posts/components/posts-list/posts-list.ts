3f456892b56016c4304113c70db6f90fd2ddefbf
dabuk58 <kubad58@onet.pl>
2025-09-18 15:42:54 +0200

    feat: posts list implementation

--- src/app/features/posts/components/posts-list/posts-list.ts
+++ src/app/features/posts/components/posts-list/posts-list.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+import { PostsStore } from '../../../../core/services/posts-store';
 
 @Component({
   selector: 'app-posts-list',
-  imports: [],
+  imports: [MatCardModule, MatIconModule, RouterLink, MatButtonModule],
   templateUrl: './posts-list.html',
-  styleUrl: './posts-list.css'
+  styleUrl: './posts-list.css',
 })
 export class PostsList {
+  readonly store = inject(PostsStore);
 
+  isFav(id: number): boolean {
+    return this.store.favorites().has(id);
+  }
+
+  toggleFav(id: number): void {
+    this.store.toggleFavorite(id);
+  }
 }
