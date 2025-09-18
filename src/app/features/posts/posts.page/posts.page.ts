a7018a97f669847f07efc6d061a354610318fdd6
dabuk58 <kubad58@onet.pl>
2025-09-18 14:49:59 +0200

    feat: posts page template and logic implementation

--- src/app/features/posts/posts.page/posts.page.ts
+++ src/app/features/posts/posts.page/posts.page.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { PostsStore } from '../../../core/services/posts-store';
+import { Spinner } from '../../../shared/ui/spinner/spinner';
+import { PostsFilters } from '../components/posts-filters/posts-filters';
+import { PostsList } from '../components/posts-list/posts-list';
 
 @Component({
   selector: 'app-posts.page',
-  imports: [],
+  imports: [PostsFilters, PostsList, Spinner],
   templateUrl: './posts.page.html',
-  styleUrl: './posts.page.css'
+  styleUrl: './posts.page.css',
 })
 export class PostsPage {
-
+  readonly store = inject(PostsStore);
 }
