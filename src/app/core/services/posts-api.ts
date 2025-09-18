6b815541f53c8ffd3c68bc60c4cea3ea1e7807ab
dabuk58 <kubad58@onet.pl>
2025-09-18 13:57:01 +0200

    feat: implement PostsApi service

--- src/app/core/services/posts-api.ts
+++ src/app/core/services/posts-api.ts
@@ -1,8 +1,29 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from '../models/post';
+import { User } from '../models/user';
+import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PostsApi {
-  
+  private http = inject(HttpClient);
+
+  getPosts(params?: { userId?: number | null }): Observable<Post[]> {
+    let httpParams = new HttpParams();
+    if (params?.userId != null) {
+      httpParams = httpParams.set('userId', String(params.userId));
+    }
+    return this.http.get<Post[]>(`${environment.apiUrl}/posts`, { params: httpParams });
+  }
+
+  getUser(userId: number): Observable<User> {
+    return this.http.get<User>(`${environment.apiUrl}/users/${userId}`);
+  }
+
+  getComments(postId: number): Observable<Comment[]> {
+    return this.http.get<Comment[]>(`${environment.apiUrl}/posts/${postId}/comments`);
+  }
 }
